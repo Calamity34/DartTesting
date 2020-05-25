84624b6e0e28daddd04e7eb4854120108b3f76ec
Calamity34 <NikitaYoutube2008@icloud.com>
2020-05-25 18:55:10 +0500

    HW and Data Types

---
+++ datatypes.dart
@@ -0,0 +1,12 @@
+main(){
+    int i = 3;
+    print(i);
+    var v = "hi";
+    print(v);
+    String s = "hello";
+    print(s);
+    bool b = true;
+    print(b);
+    double f = 3.14;
+    print(f);
+}
