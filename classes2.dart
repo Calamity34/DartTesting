30db0fca5f05572c305445501ba17337bfe3cc68
Calamity34 <NikitaYoutube2008@icloud.com>
2020-05-26 20:38:26 +0500

    classes again and string transformation

---
+++ classes2.dart
@@ -0,0 +1,14 @@
+class Linus {
+    // a thing
+    String techTips = "YES";
+
+    // le function using the thing
+    void areThereTechTips() {
+        print(techTips);
+    }
+}
+
+void main() {
+    Linus linus = new Linus();
+    linus.areThereTechTips();
+}
---
+++ toString.dart
@@ -0,0 +1,7 @@
+main(){
+    int n=34;
+    print(n.toString());
+    // The toString function can to used to convert 
+    // data types into a string in case you need to 
+    // interpolate.
+}
