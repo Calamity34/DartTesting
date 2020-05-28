52157e718bb41c9ee378044646d9562b4bb623f4
Calamity34 <NikitaYoutube2008@icloud.com>
2020-05-28 14:23:05 +0500

    cannot divide by zero, dumbass

---
+++ exceptions.dart
@@ -0,0 +1,13 @@
+main() { 
+   int x = 12; 
+   int y = 0; 
+   int res;  
+   
+   try {
+      res = x ~/ y;
+      print(res);
+   } 
+   on IntegerDivisionByZeroException { 
+      print('Cannot divide by zero, dumbass'); 
+   } 
+}
